863bbb32a290cdf2b9a26f1d30a0cb77f5efb9d5
Roger J. Romero <rjro@me.com>
2018-03-13 14:35:54 -0700

    First run

---
+++ src/WordFormatter.java
@@ -0,0 +1,9 @@
+
+public class WordFormatter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Starting work on the word formatter now...");
+	}
+
+}
