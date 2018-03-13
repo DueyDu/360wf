1215adf86c262ad37a787a8b7d4681313faf7dfb
Roger J. Romero <rjro@me.com>
2018-03-13 14:44:47 -0700

    Added sample text

--- src/WordFormatter.java
+++ src/WordFormatter.java
@@ -4,6 +4,24 @@ public class WordFormatter {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Starting work on the word formatter now...");
+		
+		
+		
+		
+	}
+	
+	/* I am writing the methods here for now for ease of access
+	 * But will refactor them to their own class later
+	 */
+	
+	public static double averageWordsPerLine(String text) {
+		return 0;
+		
+	}
+	
+	public static double averageLineLength(String text) {
+		return 0;
+		
 	}
 
 }
