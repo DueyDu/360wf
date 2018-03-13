2ba2b512fa0b876a94fd490a6e81e67f8873d518
Roger J. Romero <rjro@me.com>
2018-03-13 14:57:23 -0700

    Basic analysis added

--- src/WordFormatter.java
+++ src/WordFormatter.java
@@ -1,3 +1,9 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import sun.misc.IOUtils;
+
 
 public class WordFormatter {
 
@@ -6,6 +12,18 @@ public class WordFormatter {
 		System.out.println("Starting work on the word formatter now...");
 		
 		
+		//Replace this later with file opening
+		try {
+			String sampleText = new String(Files.readAllBytes(Paths.get("sample.txt")));
+			//System.out.println(sampleText);
+			
+			averageWordsPerLine(sampleText);
+			averageLineLength(sampleText);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 	}
@@ -15,12 +33,44 @@ public class WordFormatter {
 	 */
 	
 	public static double averageWordsPerLine(String text) {
-		return 0;
+		
+		String[] lines = text.split("\n");
+		
+		
+		double lineCount = (double)lines.length;
+		double wordCount = 0;
+		
+		for(int i=0;i<lineCount;i++) {
+			String curLine = lines[i];
+			String[] words = curLine.split(" ");
+			wordCount = wordCount + words.length;
+		}
+		
+		double averageWordsPerLine = wordCount/lineCount;
+		
+		System.out.println("Average words per line: " + averageWordsPerLine);
+		
+		return averageWordsPerLine;
 		
 	}
 	
 	public static double averageLineLength(String text) {
-		return 0;
+		
+		String[] lines = text.split("\n");
+		
+		double lineCount = (double)lines.length;
+		double totalLineLengths = 0;
+		
+		for(int i=0;i<lineCount;i++) {
+			double curLineLength = (double)lines[i].length();
+			totalLineLengths = totalLineLengths + curLineLength;
+		}
+		
+		double averageLineLength = totalLineLengths/lineCount;
+		
+		//This is length in characters, not words
+		System.out.println("Average line length: " + averageLineLength);
+		return averageLineLength;
 		
 	}
 
